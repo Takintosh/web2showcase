724da2c4efacc1deb02d25aa71c12e3072844e5c
Amilcar Santana <takintosh@gmail.com>
2024-04-18 16:49:11 -0300

    First Commit

---
+++ src/main/java/com/takintosh/web2showcase/Web2CarShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.takintosh.web2showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2CarShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2CarShowcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takintosh/web2showcase/Web2CarShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takintosh.web2showcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2CarShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
