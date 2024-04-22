a7aec4fdd21abca5616e96e903b5251b0af3a89e
Amilcar Santana <takintosh@gmail.com>
2024-04-21 21:57:45 -0300

    Update CategoryRecordDto.java

--- src/main/java/com/takintosh/web2showcase/dtos/CategoryRecordDto.java
+++ src/main/java/com/takintosh/web2showcase/dtos/CategoryRecordDto.java
@@ -3,7 +3,7 @@ package com.takintosh.web2showcase.dtos;
 import jakarta.validation.constraints.NotBlank;
 
 public record CategoryRecordDto(
-        @NotBlank String name
+        @NotBlank String categoryName
 ) {
 
 }
